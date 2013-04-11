569af2049157b740061bf12d33fac8467532a2c1
SaltireSoul <git@archtechnologies.co.uk>
2013-04-11 18:19:22 +0100

    initial commit v1

---
+++ background.js
@@ -0,0 +1,54 @@
+if (webkitNotifications) {
+
+// Helper function to show a notification
+function showNotification(extension) {
+
+  if (extension) {
+    // Save temporary the old version to display it in the notification
+    extension.oldVersion = localStorage[extension.id];
+  }
+  
+  var icon = '';
+  if (extension.icons && extension.icons.length !== 0) {
+    icon = extension.icons[extension.icons.length-1].url;
+  } else {
+    icon = 'chrome://extension-icon/' + extension.id + '/32/0';
+  }
+  
+  var title = '';
+  title = extension.name + ' Updated';
+  
+  var text = '';
+  text = 'From: ' + extension.oldVersion + '\nTo: ' + extension.version;
+
+  var notification = webkitNotifications.createNotification(icon,title,text);
+  notification.show();
+}
+
+function checkExtensionVersion(extension) {
+
+  if (localStorage[extension.id] && (localStorage[extension.id] != extension.version) ) {
+    showNotification(extension);
+  }
+    
+  // Store new version of this extension
+  localStorage[extension.id] = extension.version;
+}
+
+chrome.management.getAll(function (extensions) {
+
+  // Store versions of installed extensions once
+  extensions.forEach(function (extension) {
+    checkExtensionVersion(extension);
+  });
+
+  // Show a notification each time a new version of an extension is installed
+  chrome.management.onInstalled.addListener(function (extension) {
+    checkExtensionVersion(extension);
+  });
+});
+
+} else {
+    // Show a new tab with an error message.
+    chrome.tabs.create({url: 'error.html'});
+}
